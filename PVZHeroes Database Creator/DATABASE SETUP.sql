057e7e21c1b8f1ad6ab65c5c1e6d0b8b35c75e52
Aitor Rodríguez <aitorrod@ucm.es>
2021-02-02 01:31:56 +0100

    Commit inicial

---
+++ PVZHeroes Database Creator/DATABASE SETUP.sql
@@ -0,0 +1,21 @@
+CREATE USER 'webuser'@'localhost' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON * . * TO 'webuser'@'localhost';
+
+CREATE SCHEMA `pvzheroes` ;
+
+CREATE TABLE `pvzheroes`.`cartas` (
+  `Nombre` VARCHAR(30) NOT NULL,
+  `Ataque` INT NULL,
+  `Defensa` INT NULL,
+  `Coste` INT NULL,
+  `Clase` TINYTEXT NULL,
+  `Tribus` TINYTEXT NULL,
+  `Atributos` TINYTEXT NULL,
+  `Habilidades` TINYTEXT NULL,
+  `Rareza` TINYTEXT NULL,
+  `Mazo` TINYTEXT NULL,
+  `Tipo` TINYTEXT NULL,
+  `URL` TEXT NULL,
+  `NumeroAtributos` INT NULL,
+  PRIMARY KEY (`Nombre`));
