1fb52dd3befab18d4284f374a1917c6258015287
Aitor Rodríguez <aitorrod@ucm.es>
2021-02-13 03:59:45 +0100

    Nuevo inicio y cambios en el estilo de columnas

--- PVZHeroes-Stats (Web App)/src/main/java/org/com/PVZHeroesStatswebapp/Cards/Cartas.java
+++ PVZHeroes-Stats (Web App)/src/main/java/org/com/PVZHeroesStatswebapp/Cards/Cartas.java
@@ -1,5 +1,6 @@
 package org.com.PVZHeroesStatswebapp.Cards;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -10,14 +11,40 @@ public class Cartas {
 	private String Nombre;
 	private int Ataque;
 	private int Defensa;
+	private int Coste;
+	private String Clase;
+	private String Tribus;
+	private String Atributos;
+	private String Habilidades;
+	private String Rareza;
+	private String Mazo;
+	private String URL;
+	private String Tipo;
+	private int NumeroAtributos;
+	private String Imagen;
 	
-	protected Cartas() {}
-			
-	public Cartas(String nombre, int ataque, int defensa) {
+	
+	public Cartas(String nombre, int ataque, int defensa, int coste, String clase, String tribus, String atributos,
+			String habilidades, String rareza, String mazo, String tipo, String uRL, int numeroAtributos, String imagen) {
 		Nombre = nombre;
 		Ataque = ataque;
 		Defensa = defensa;
+		Coste = coste;
+		Clase = clase;
+		Tribus = tribus;
+		Atributos = atributos;
+		Habilidades = habilidades;
+		Rareza = rareza;
+		Mazo = mazo;
+		Tipo = tipo;
+		URL = uRL;
+		NumeroAtributos = numeroAtributos;
+		Imagen = imagen;
 	}
+
+
+	public Cartas() {}
+
 	
 	public String getNombre() {
 		return Nombre;
@@ -37,9 +64,93 @@ public class Cartas {
 	public void setDefensa(int defensa) {
 		Defensa = defensa;
 	}
+	
+	public int getCoste() {
+		return Coste;
+	}
 
-	@Override
-	public String toString() {
-		return "Card [Nombre=" + Nombre + ", Ataque=" + Ataque + ", Defensa=" + Defensa + "]";
-	}	
+	public void setCoste(int coste) {
+		Coste = coste;
+	}
+
+	public String getClase() {
+		return Clase;
+	}
+
+	public void setClase(String clase) {
+		Clase = clase;
+	}
+
+	public String getTribus() {
+		return Tribus;
+	}
+
+	public void setTribus(String tribus) {
+		Tribus = tribus;
+	}
+
+	public String getAtributos() {
+		return Atributos;
+	}
+
+	public void setAtributos(String atributos) {
+		Atributos = atributos;
+	}
+
+	public String getHabilidades() {
+		return Habilidades;
+	}
+
+	public void setHabilidades(String habilidades) {
+		Habilidades = habilidades;
+	}
+
+	public String getRareza() {
+		return Rareza;
+	}
+
+	public void setRareza(String rareza) {
+		Rareza = rareza;
+	}
+
+	public String getMazo() {
+		return Mazo;
+	}
+
+	public void setMazo(String mazo) {
+		Mazo = mazo;
+	}
+
+	public String getTipo() {
+		return Tipo;
+	}
+
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
+	public int getNumeroAtributos() {
+		return NumeroAtributos;
+	}
+
+	public void setNumeroAtributos(int numeroAtributos) {
+		NumeroAtributos = numeroAtributos;
+	}
+	
+	public String getImagen() {
+		return Imagen;
+	}
+	public void setImagen(String imagen) {
+		Imagen = imagen;
+	}
+
+
 }
--- PVZHeroes-Stats (Web App)/src/main/java/org/com/PVZHeroesStatswebapp/Controller/MainController.java
+++ PVZHeroes-Stats (Web App)/src/main/java/org/com/PVZHeroesStatswebapp/Controller/MainController.java
@@ -1,26 +1,50 @@
 package org.com.PVZHeroesStatswebapp.Controller;
 
-
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 import org.com.PVZHeroesStatswebapp.Cards.Cartas;
+import org.com.PVZHeroesStatswebapp.Service.CardsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("/index")
-	public String showHelloWorld(Model theModel) {	
-		Cartas carta1 = new Cartas("Carta 1", 1, 1);
-		Cartas carta2 = new Cartas("Carta 2", 2, 1);
-		Cartas carta3 = new Cartas("Carta 3", 4, 5);
-		ArrayList<Cartas> Cards = new ArrayList();
-		Cards.add(carta1);
-		Cards.add(carta2);
-		Cards.add(carta3);
-		theModel.addAttribute("cards",Cards);
-		return "helloworld";
-	}	
+	@Autowired
+	private CardsService cardsService;
 
+	@GetMapping("/index")
+	public String mostrarPaginaInicial(Model theModel) {
+		añadirListadoCompletoAlModelo(theModel);
+		theModel.addAttribute("cartaBuscada", new Cartas());
+		return "index";
+	}
+
+	@RequestMapping("/busqueda")
+	public String mostrarPaginaBusqueda(@ModelAttribute("cartaBuscada") Cartas cartaBuscada,
+		 Model theModel) {
+		String nombreCarta = cartaBuscada.getNombre().trim();
+		try {
+			if (nombreCarta.equals("")) {
+				añadirListadoCompletoAlModelo(theModel);
+				return "index";
+			} else {
+				Cartas cartaRecuperada = cardsService.findById(nombreCarta);
+				theModel.addAttribute("cartaBuscada", cartaRecuperada);
+				return "busqueda";
+			}
+		} catch (NoSuchElementException ex) {
+			theModel.addAttribute("nombreCarta", nombreCarta);
+			return "busquedaFallida";
+		}
+	}
+
+	private void añadirListadoCompletoAlModelo(Model theModel) {
+		ArrayList<Cartas> cartas = cardsService.findAll();
+		theModel.addAttribute("cards", cartas);
+	}
 }
