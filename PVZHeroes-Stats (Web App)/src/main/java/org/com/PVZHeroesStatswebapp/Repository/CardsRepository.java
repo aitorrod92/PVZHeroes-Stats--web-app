c96dbee4de39b377109c7b752c6d590fdad85b17
Aitor Rodríguez <aitorrod@ucm.es>
2021-03-11 23:59:11 +0100

    Refactoriza las plantillas y las organiza en fragmentos. Elimina la pagina de busqueda

--- PVZHeroes-Stats (Web App)/src/main/java/org/com/PVZHeroesStatswebapp/Repository/CardsRepository.java
+++ PVZHeroes-Stats (Web App)/src/main/java/org/com/PVZHeroesStatswebapp/Repository/CardsRepository.java
@@ -1,19 +1,9 @@
 package org.com.PVZHeroesStatswebapp.Repository;
 
-import java.util.ArrayList;
-
 import org.com.PVZHeroesStatswebapp.Entities.Cartas;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CardsRepository extends JpaRepository<Cartas, String>, UserRepositoryCustom {
 
-	/*
-	@Query("SELECT u FROM Cartas u WHERE u.Nombre LIKE %:id%")
-	ArrayList<Cartas> findByPatternIdLIKE(@Param("id") String id);
-	
-	@Query("SELECT u FROM Cartas u WHERE u.Nombre NOT LIKE %:id%") 
-	ArrayList<Cartas> findByPatternIdNOTLIKE(@Param("id") String id);*/
 }
 
